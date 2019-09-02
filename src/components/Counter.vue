a6f965813157080c635b74c8d5afffac56404d61
zhangjing <juslly@163.com>
2019-09-02 11:28:24 +0800

    push counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="hello">
+<h1>{{ msg }}</h1>
+<button type="button" v-on:click="_add">+</button>
+<button type="button" v-on:click="_minus">-</button>
+<p>{{number}}</p >
+</div>
+</template>
+
+<script>
+export default {
+name: 'Counter',
+props: {
+msg: String
+},
+
+data(){
+return {
+number: 0
+}
+},
+methods:{
+_add: function (){
+this.number ++;
+},
+
+_minus: function (){
+this.number --;
+}
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+margin: 40px 0 0;
+}
+ul {
+list-style-type: none;
+padding: 0;
+}
+li {
+display: inline-block;
+margin: 0 10px;
+}
+a {
+color: #42b983;
+}
+</style>
